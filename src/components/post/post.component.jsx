6bc3102499fe7fa220065010d35d45d943d237d7
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-04 21:25:08 -0300

    first commit

---
+++ src/components/post/post.component.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+
+import Card from '../card/card.component';
+
+const Post = ({ postId }) => {
+  
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+      const fetchPost = async() => {
+      const res_posts = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${postId}`);
+      const posts = await res_posts.json()
+      setPost(posts[0]);
+    }
+
+    fetchPost()
+  })
+
+  return (
+    <Card>
+      {post ? (
+        <div>
+          <h3> {post.title} </h3>
+          <p> {post.body} </p>
+        </div>
+      ) : (
+        <p> No post found </p>
+      )}
+    </Card>
+  );
+};
+
+export default Post;
---
+++ src/components/user/user.component.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+
+import Card from '../card/card.component';
+
+const User = ({ userId }) => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+      const fetchUser = async() => {
+      const res_users = await fetch(`https://jsonplaceholder.typicode.com/users?id=${userId}`);
+      const users = await res_users.json()
+      setUser(users[0]);
+    }
+
+    fetchUser();
+  })
+
+  return (
+    <Card>
+      {user ? (
+        <div>
+          <h3>{user.username}</h3>
+          <p>{user.name}</p>
+        </div>
+      ) : (
+        <p>User not found</p>
+      )}
+    </Card>
+  );
+};
+
+export default User;
