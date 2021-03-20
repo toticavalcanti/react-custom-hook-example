10ab3d3202c799fee4bc9ba3d45a030984aa0c12
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-03-20 17:39:57 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-
-import User from './components/User/user.component';
-import Post from './components/Post/post.component';
+import UsersGallery from './components/UsersGallery/usersGallery.component';
+//import User from './components/user/user.component';
+//import Post from './components/Post/post.component';
 
 import './App.css';
 
 const App = props => {
   return (
     <div className='App'>
-      <User userId={5} />
-      <Post postId={15} />
+      <UsersGallery />
+      {/* <Post postId={15} /> */}
     </div>
   );
 };
-
 export default App;
---
+++ src/components/UsersGallery/usersGallery.component.jsx
@@ -0,0 +1,17 @@
+import React, { memo } from  'react';
+import useFetch from '../../effects/use-fetch.effect';
+import User from '../User/user.component';
+import './usersGallery.css';
+
+function UsersGallery(){
+
+  const users = useFetch(
+      `https://jsonplaceholder.typicode.com/users`
+  );
+  return(   
+    <div className='container'>
+        {Array.isArray(users) && users.map((user) => <User user={user} key={user.id} />)}
+    </div>
+ )
+}
+export default memo(UsersGallery);
